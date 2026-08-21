c91634e26e6c5e50b7e7cd4a1a23ceeb9913b87d
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 15:50:55 +0200

    Remove source comments and DocC

--- Sources/Cardinal Algebra Primitives/exports.swift
+++ Sources/Cardinal Algebra Primitives/exports.swift
@@ -1,4 +1,2 @@
-// exports.swift
-
 @_exported public import Algebra_Semiring_Primitives
 @_exported public import Cardinal_Primitives
--- Tests/Support/Cardinal Algebra Primitives Test Support.swift
+++ Tests/Support/Cardinal Algebra Primitives Test Support.swift
@@ -1,5 +1,1 @@
-// Cardinal Algebra Primitives Test Support
-
 internal import Cardinal_Algebra_Primitives
-
-// Cardinal already has ExpressibleByIntegerLiteral in Cardinal Primitives Standard Library Integration.
